cdefead3883ccba9a032302eccab033143987734
ir-2153 <91446114+ir-2153@users.noreply.github.com>
2022-11-11 20:36:22 -0800

    Cleaned up code

--- ConsoleApplication5.cpp
+++ ConsoleApplication5.cpp
@@ -1,6 +1,3 @@
-// hahhahahahahahahahah.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <Windows.h>
 #define MYRGB(r,g,b) (r | g << 8 | b << 16)
@@ -14,10 +11,6 @@ int main()
 
 		HDC hdc;
 		hdc = GetWindowDC(GetDesktopWindow());
-
-		int y = GetSystemMetrics(0);
-		int x = GetSystemMetrics(1);
-
 		int sw = GetSystemMetrics(0);
 		int sh = GetSystemMetrics(1);
 		SetTextColor(hdc, MYRGB(0, 0, 255));
@@ -25,17 +18,5 @@ int main()
 		TextOutW(hdc, rand() % sw, rand() % sh, lptext, wcslen(lptext));
 		TextOutW(hdc, rand() % sw, rand() % sh, lptext, wcslen(lptext));
 		TextOutW(hdc, rand() % sw, rand() % sh, lptext, wcslen(lptext));
-		Sleep(1);
 	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
